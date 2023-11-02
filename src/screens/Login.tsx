d1e137ee369d102873a0b34d2e788d21239bc378
Flora-Pvt <flora.preuvot@autodesk.com>
2023-11-02 17:20:13 +0100

    feat(login): add login screen

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,10 +1,56 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react'
+import { View, TextInput, Pressable, Text } from 'react-native'
 
-export default function Login() {
-  return (
-    <View>
-      <Text>Sign in</Text>
-    </View>
-  )
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+
+interface LoginState {
+  name: string
+  email: string
+  password: string
 }
+
+export class Login extends Component<{}, LoginState> {
+  constructor(props) {
+    super(props)
+
+    this.onSignin = this.onSignin.bind(this)
+  }
+
+  onSignin = () => {
+    const auth = getAuth()
+    const { name, email, password } = this.state
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput
+          placeholder="Name"
+          onChangeText={(name) => this.setState({ name })}
+        />
+        <TextInput
+          placeholder="Email"
+          onChangeText={(email) => this.setState({ email })}
+        />
+        <TextInput
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={(password) => this.setState({ password })}
+        />
+        <Pressable onPress={this.onSignin}>
+          <Text>Sign In</Text>
+        </Pressable>
+      </View>
+    )
+  }
+}
+
+export default Login
